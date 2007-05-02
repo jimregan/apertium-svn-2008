dda2d3cd429e782f1c646f96700e8d3036eaefb9
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2007-05-02 10:33:39 +0000

    Fixing codingstyle
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium@244 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium-en-de/wordmirror/printnounxml.py
+++ apertium-en-de/wordmirror/printnounxml.py
@@ -1,55 +1,61 @@
 # Francis Tyers
 
-from WordMirror import WordMirrorNounInflection
+from WordMirror import WordMirrorNounInflection;
 
-def print_xml(inflection, noun): 
+def print_xml(inflection, noun): #{
 
-	casesymbols = { "Nominative": "nom",
-	                "Dative": "dat",
-	                "Accusative": "acc",
-	                "Genitive": "gen" }
+	casesymbols = { "Nominative": "nom", "Dative": "dat", "Accusative": "acc", "Genitive": "gen" };
 
-	numbersymbols = { "No Article": "sg",
-	                  "Plural No Article": "pl" }
+	numbersymbols = { "No Article": "sg", "Plural No Article": "pl" };
 
 	print '    <pardef n="' + noun +  '__n">';
 
 	gender = inflection['Gender'];
-	if(gender == 'n'): 
+	if(gender == 'n'): #{
 		gender = 'nt';
+	#}
 	
-	for key in inflection.keys(): 
-		if(key in ['Gender', 'Word Type', 'Plural']): 
-			continue
+	for key in inflection.keys(): #{
+		if(key in ['Gender', 'Word Type', 'Plural']): #{
+			continue;
+		#}
 
-		for (case, infl1) in inflection[key].items(): 
-			if(key not in numbersymbols.keys()):
-				continue
+		for (case, infl1) in inflection[key].items(): #{
+			if(key not in numbersymbols.keys()): #{
+				continue;
+			#}
 
-			for infl2 in infl1.split("/"):
-				print '      <e> <!-- ' + key + ' -->'
-				print '        <p>'
+			for infl2 in infl1.split("/"): #{
+				print '      <e> <!-- ' + key + ' -->';
+				print '        <p>';
 
-				if(infl2 == noun): 
-					print '          <l/>'
-				else: 
-					print '          <l>' + infl2.replace(noun, '') + '</l>'
+				if(infl2 == noun): #{
+					print '          <l/>';
+				#}
+				if(infl2 != noun): #{
+					print '          <l>' + infl2.replace(noun, '') + '</l>';
+				#}
 
-				print '          <r><s n="n"/><s n="' + gender + '"/><s n="' + casesymbols[case] +'"/><s n="' + numbersymbols[key] + '"/></r>'
-				print '        </p>'
-				print '      </e>'
+				print '          <r><s n="n"/><s n="' + gender + '"/><s n="' + casesymbols[case] +'"/><s n="' + numbersymbols[key] + '"/></r>';
+				print '        </p>';
+				print '      </e>';
+			#}
+		#}
+	#}
 	
-	print '    </pardef>'
+	print '    </pardef>';
 
-def usage(): 
-	print 'python printnounxml.py <noun>'
+def usage(): #{
+	print 'python printnounxml.py <noun>';
+#}
 
-if __name__ == "__main__":
-	import sys
+if __name__ == "__main__": #{
+	import sys;
 
-	if(len(sys.argv) <= 1): 
-		usage()
-		sys.exit()
-	
-	inflect = WordMirrorNounInflection(sys.argv[1])
-	print_xml(inflect, sys.argv[1])
+	if(len(sys.argv) <= 1): #{
+		usage();
+		sys.exit();
+	#}
+	inflect = WordMirrorNounInflection(sys.argv[1]);
+	print_xml(inflect, sys.argv[1]);
+#}
