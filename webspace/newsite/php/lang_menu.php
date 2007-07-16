16f793f7bc0c6e4fd0fc24dccb82dfce52c6ee38
ebenimeli <ebenimeli@72bbbca6-d526-0410-a7d9-f06f51895060>
2007-07-16 14:27:31 +0000

    changes in navigation menu
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium@896 72bbbca6-d526-0410-a7d9-f06f51895060

--- webspace/newsite/php/lang_menu.php
+++ webspace/newsite/php/lang_menu.php
@@ -14,22 +14,22 @@ function printLanguageLink($code, $text, $url, $newLang) {
 <div id="langselect">
 	<?php
 		//printLanguageLink("af","afrikaans (afrikaans)", $url, $newLang, $currentLang);
-		printLanguageLink("ca","català", $url, $newLang, $currentLang);
+		printLanguageLink("ca","catal&agrave;", $url, $newLang, $currentLang);
 		//printLanguageLink("cy","cymraeg", $url, $newLang, $currentLang);
 		//printLanguageLink("da","dansk", $url, $newLang, $currentLang);
-		printLanguageLink("de","deutsch", $url, $newLang, $currentLang);		
+		//printLanguageLink("de","deutsch", $url, $newLang, $currentLang);		
 		printLanguageLink("en","english", $url, $newLang, $currentLang);
-		printLanguageLink("es","español", $url, $newLang, $currentLang);
-		printLanguageLink("eu","euskara", $url, $newLang, $currentLang);		
-		printLanguageLink("fr","français", $url, $newLang, $currentLang);
+		printLanguageLink("es","espa&ntilde;ol", $url, $newLang, $currentLang);
+		//printLanguageLink("eu","euskara", $url, $newLang, $currentLang);		
+		printLanguageLink("fr","fran&ccedil;ais", $url, $newLang, $currentLang);
 		printLanguageLink("gl","galego", $url, $newLang, $currentLang);
 		//printLanguageLink("id","indonesian", $url, $newLang, $currentLang);
 		//printLanguageLink("mk","македонски јазик (macedonian)", $url, $newLang, $currentLang);		
 		//printLanguageLink("ms","bahasa Melayu (malay)", $url, $newLang, $currentLang);		
 		//printLanguageLink("nl","nederlands (	dutch)", $url, $newLang, $currentLang);
-		printLanguageLink("oc","occità", $url, $newLang, $currentLang);
-		printLanguageLink("pt","português", $url, $newLang, $currentLang);
-		printLanguageLink("ro","română", $url, $newLang, $currentLang);
+		printLanguageLink("oc","occit&agrave;", $url, $newLang, $currentLang);
+		printLanguageLink("pt","portugu&ecirc;s", $url, $newLang, $currentLang);
+		//printLanguageLink("ro","rom&acirc;nă", $url, $newLang, $currentLang);
 		//printLanguageLink("sh","cрпскохрватски / srpskohrvatski (serbo-croatian)", $url, $newLang, $currentLang);
 		//printLanguageLink("sv","svenska (swedish)", $url, $newLang, $currentLang);
 		//printLanguageLink("tg","тоҷикӣ (tajik)", $url, $newLang, $currentLang);		
