a82a81a9bb9f28f86e9eb6aa3d542ef69bd8d730
ebenimeli <ebenimeli@72bbbca6-d526-0410-a7d9-f06f51895060>
2007-09-18 10:25:06 +0000

    New website moved to root folder
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium@1353 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ webspace/oldsite/index.php
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta name="generator" content=
+  "HTML Tidy for MkLinux (vers 1st August 2004), see www.w3.org" />
+
+  <title>Apertium: an open-source shallow-transfer machine
+  translation engine and toolbox</title>
+<style type="text/css">
+/*<![CDATA[*/
+   body {background : white }
+   h1, h2 { font-family : sans-serif }
+   td { font-family : sans-serif }
+   img { vertical-align : text-top }
+   ul { list-style-type : disc }
+   .novetat { background : yellow ; color : purple ; border : black ;
+margin-bottom : 0.5ex ; border : solid ; border-width : thin }
+   .nou { background : blue ; color : yellow }
+   a { text-decoration : none ; font-style : italic }
+   a:hover { color : red ; background : yellow  }
+   .box { background : #EEFFFF ; border : solid ; border-width : thin }
+/*]]>*/
+</style>
+<style type="text/css">
+/*<![CDATA[*/
+ div.c1 {text-align: center}
+/*]]>*/
+</style>
+</head>
+
+<body>
+  <h1>Apertium: an open-source shallow-transfer machine translation
+  engine and toolbox</h1>
+
+  <h2>What is Apertium and who is developing it</h2>
+
+  <p><strong>Apertium</strong> is one of the two open-source
+  machine translation engines being developed inside project
+  <a href="http://www.opentrad.org">OpenTrad</a>; Apertium is
+  designed to translate between related languages (see
+  Documentation below). The OpenTrad consortium is led by <a href=
+  "http://www.eleka.net">Eleka Ingeniaritza Linguistikoa</a>.</p>
+
+  <p>The project ("Open Source Machine Translation for the Romance
+  languages of Spain") is funded by the <a href=
+  "http://www.min.es">Ministry of Industry, Tourism and
+  Commerce</a> of Spain trough <a href=
+  "http://www.mityc.es/profit/">PROFIT</a> grant number
+  FIT340101-2004-0003.</p>
+
+  <div class="c1">
+    <img src=
+    "http://www2.mityc.es/MitycCMS/images/LogoMinisterio.jpg" alt=
+    "Ministerio de Industria, Turismo y Comercio" />
+  </div>
+
+  <p>The Apertium architecture is being developed by the <a href=
+  "http://transducens.dlsi.ua.es">Transducens</a> research group at
+  the <a href="http://www.dlsi.ua.es">Departament de Llenguatges i
+  Sistemes Informàtics</a> of the <a href=
+  "http://www.ua.es/">Universitat d'Alacant</a>. Linguistic data
+  for Apertium are being developed by Tranducens (Spanish-Catalan),
+  and the <a href="http://webs.uvigo.es/sli/">Seminario de
+  Lingüística Informàtica</a> of the <a href=
+  "http://www.uvigo.es/indice/index.gl.htm">Universidade de
+  Vigo</a> (Spanish-Galician).</p>
+
+  <h2>Software</h2>
+
+  <p>Apertium (packages <code>lttoolbox</code> and
+  <code>apertium</code>) as well as the corresponding language-pair
+  data will be released through SourceForge page <a href=
+  "http://sf.net/projects/apertium/">sf.net/projects/apertium/</a>.
+  The expected date of release for version 0.8 and linguistic data
+  for Spanish-Catalan is July 29, 2005.</p>
+
+  <h2>Test drive</h2>
+
+  <p>Before installing Apertium, you will be able to evaluate it
+  online. The Spanish-Catalan system will be made available on July
+  29, 2005 at <a href=
+  "http://xixona.dlsi.ua.es/prototype">xixona.dlsi.ua.es/prototype/</a>.</p>
+
+  <h2>Documentation</h2>
+
+  <p>Documentation in Spanish (detailed but still incomplete) and
+  <a href=
+  "http://sourceforge.net/docman/display_doc.php?docid=29380&amp;group_id=143781">
+  English</a> (still very preliminary) is available through the
+  <a href=
+  "http://sf.net/projects/apertium/">sf.net/projects/apertium/</a>
+  project page.</p>
+
+  <p>We have published two conference papers describing the
+  architecture and its motivation:</p>
+
+  <ul>
+    <li><a href=
+    "http://www.dlsi.ua.es/~japerez/pub/pdf/eamt2005.pdf">An
+    open-source shallow-transfer machine translation engine for the
+    romance languages of Spain</a> (by Antonio M. Corbí-Bellot,
+    Mikel L. Forcada, Sergio Ortiz-Rojas, Juan Antonio Pérez-Ortiz,
+    Gema Ramírez-Sánchez, Felipe Sánchez-Martínez, Iñaki Alegria,
+    Aingeru Mayor and Kepa Sarasola, in Proceedings of the <a href=
+    "http://www.eamt.org/eamt2005/">European Association for
+    Machine Translation, 10th Annual Conference</a>, Budapest 2005,
+    p. 79-86.)</li>
+
+    <li><a href=
+    "http://www.dlsi.ua.es/~mlf/docum/armentano05p.pdf">An
+    open-source shallow-transfer machine translation toolbox:
+    consequences of its release and availability</a> (by Carme
+    Armentano-Oller, Antonio M. Corbí-Bellot, Mikel L. Forcada,
+    Mireia Ginestí-Rosell, Boyan Bonev, Sergio Ortiz-Rojas, Juan
+    Antonio Pérez-Ortiz, Gema Ramírez-Sánchez, Felipe
+    Sánchez-Martínez, accepted at OSMaTran: Open-Source Machine
+    Translation, A workshop at <a href=
+    "http://www.tcllab.org/Pages/mtsummit.html">Machine Translation
+    Summit X</a>, Phuket, Thailand 2005).</li>
+  </ul>
+  <hr />
+</body>
+</html>
