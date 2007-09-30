4ad1529a5c620de97f9f8ead464777a8d67e02c2
naudefj <naudefj@72bbbca6-d526-0410-a7d9-f06f51895060>
2007-09-30 12:29:21 +0000

    Afrikaans translation
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium@1426 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ webspace/content/af/sections.php
@@ -0,0 +1,44 @@
+<?php
+	$sections = array("whatisapertium", "whodevelopsit", "downloading", "testdrive", "documentation", "interact", "latestnews", "software", "funding", "users", "contact");
+	$text = array("wat is apertium", "wie ontwikkel dit", "laai af", "toetsbestuur", "dokumentasie", "bespreek", "nuus", "sagteware", "bevondsing", "gebruikers", "kontak");
+
+$subsections = array
+(
+/*
+"whatisapertium" =>array
+   (
+   "langpairs"=>"Language pairs",
+   ),
+   */
+   "testdrive"=>array
+   (
+   	"link@Spanish&ndash;Catalan"=>"http://xixona.dlsi.ua.es/prototype",
+   	"link@Spanish&ndash;Galician"=>"http://sli.uvigo.es/tradutor"
+   ),
+"documentation" =>array
+   (
+   /*"documents"=>"Documents",*/
+   "publications"=>"Publications",
+   "link@Wiki" => "http://xixona.dlsi.ua.es/wiki"
+   ),
+"interact" =>array
+	(
+		"link@cgi::irc"=>"http://xixona.dlsi.ua.es/cgi-bin/cgiirc/irc.cgi",
+		"link@Forum"=>"http://sourceforge.net/forum/forum.php?forum_id=481030",
+		"link@Mailing list"=>"https://lists.sourceforge.net/lists/listinfo/apertium-stuff"
+	),
+"latestnews" =>array
+	(
+		"link@RSS"=>"http://www.sf.net/export/rss2_projnews.php?group_id=143781"
+	),
+"software" =>array
+	(
+		"apertium-tagger-training-tools"=>"Tagger training tools",
+		"apertium-transfer-tools"=>"Transfer tools",
+		"lttoolbox"=>"Lexical transformation",
+		"apertium-eval-translator"=>"Evaluation"
+	),
+);
+
+?>
+
