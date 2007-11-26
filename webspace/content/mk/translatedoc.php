c8072ca6e8b231abe3b533a967ff18de3fd8f453
palifra <palifra@72bbbca6-d526-0410-a7d9-f06f51895060>
2007-11-26 00:21:42 +0000

    Partialy translated files. Non-exhaustive a?
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium@2643 72bbbca6-d526-0410-a7d9-f06f51895060

--- webspace/content/mk/translatedoc.php
+++ webspace/content/mk/translatedoc.php
@@ -1,6 +1,6 @@
 <?php
 
-$SELECT_TRANS_TYPE = "Select translation type";
-$DOCUMENT = "Select the document type to be translated";
+$SELECT_TRANS_TYPE = "Избери тип на превод";
+$DOCUMENT = "Избери го типот на документот што треба да се преведува";
 
 ?>
