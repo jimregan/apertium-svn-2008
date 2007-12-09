7ffc5a840efea7dc555c8d744124ab5a153d151c
Winterstream <Winterstream@72bbbca6-d526-0410-a7d9-f06f51895060>
2007-12-09 02:20:48 +0000

    Added most of the finishing touches on the script
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium@2868 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium-en-af/find-missing-monodix-entries.py
+++ apertium-en-af/find-missing-monodix-entries.py
@@ -3,36 +3,42 @@
 # -*- encoding: utf-8 -*-
 
 import os
+import os.path as path
 import re
+import sys
 import cStringIO
+import pprint
 from itertools import izip
 from subprocess import Popen, PIPE
 
+# In order to use this script, you need to install 4Suite's cDomlette.
 from Ft.Xml.Domlette import NonvalidatingReader;
 from Ft.Xml.Domlette import Print, PrettyPrint;
 from Ft.Xml.XPath import Evaluate;
 
+"""
+"""
 
+
+# Change this for your language
 template = {
-    '<vblex>' : '<par n="breek__vblex"/>',
-    '<adj>' : '<par n="dadelik__adj"/>',
-    '<n>' : '<par n="appèl__n"/>',
+    '<vblex>'  : '<par n="breek__vblex"/>',
+    '<adj>'    : '<par n="dadelik__adj"/>',
+    '<n>'      : '<par n="appèl__n"/>',
     '<n><unc>' : '<par n="poësie__n__unc"/>',
-    '<np>' : '<par n="Engeland__np"/>',
-    '<adv>' : '<par n="miskien__adv"/>',
+    '<np>'     : '<par n="Engeland__np"/>',
+    '<adv>'    : '<par n="miskien__adv"/>',
     '<preadv>' : '<par n="taamlik__preadv"/>'
 }
 
-
+# Default global values
 class Globals:
-    lt_proc_path = '/usr/local/bin/lt-proc'
-    morpo_path = '/usr/local/share/apertium/apertium-en-af/af-en.automorf.bin'
+    lt_proc_path  = '/usr/local/bin/lt-proc'
 
+# Parse an XML document and return a DOM tree. 
 def load_xml_file(filename):
-    return NonvalidatingReader.parseUri(filename)
+    return NonvalidatingReader.parseUri('file://' + os.path.realpath(filename))
 
-def usage():
-    print "usage <monodix path> <bidix path> [<side>]"
 
 def extract_entries(doc, side='l'):
     return doc.xpath("/dictionary/section[@id='main']/e/p/" + side)
@@ -65,15 +71,24 @@ def extract_tags(s):
 
         i += 1
 
-def run_morpho(input_list):
-    proc = Popen([Globals.lt_proc_path, Globals.morpo_path], stdin=PIPE, stdout=PIPE)
-    stdout, stderr = proc.communicate("\n".join(input_list))
-    return process_output(stdout)
+def call(name, input=''):
+    proc = Popen(name, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate(input)
+    
+    if not (err == None or err == ''):
+        raise Exception(err)
 
-def find_missing_entries(lst):
+    return out
+
+# Execute lt-proc 
+def run_ltproc(morfo_path, input_list):
+    out = call([Globals.lt_proc_path, morfo_path], "\n".join(input_list))
+    return process_output(out)
+
+def find_missing_entries(morfo_path, lst):
     i = make_input_list(lst)
     c = make_compare_list(lst)
-    o = run_morpho(i)
+    o = run_ltproc(morfo_path, i)
 
     missing = []
 
@@ -89,13 +104,7 @@ def write_entries_to_file(f, lst):
     for lemma, tags in (extract_tags(e) for e in lst):
         if tags in template:
             f.write(u'    <e lm="%(lemma)s"><i>%(lemma)s</i>%(template)s</e>\n' % { 'lemma': lemma,
-                                                                           'template': template[tags] })
-
-def test_load():
-    Globals.automorf = '/home/wynand/apertium-git/apertium-en-af/af-en.automorf.bin'
-
-def test_load_file():
-    return load_xml_file('file:///home/wynand/apertium-git/apertium-en-af/apertium-en-af.en-af.dix.xml')
+                                                                                    'template': template[tags] })
 
 def node_to_string(node):
        buf = cStringIO.StringIO();
@@ -105,18 +114,84 @@ def node_to_string(node):
 def nodes_to_string(lst):
     return ''.join(node_to_string(n) for n in lst)
 
-if __name__ == '__main__' and False:
-    import sys
+
+
+##def last_component(pathname):
+##     return path.split(pathname)[2]
+
+## def get_automorf_pair(automorf_path):
+##     try:
+##         return re.match('([^.]*[.]automorf[.]bin)', last_component(automorf_path))
+##     except:
+##         raise Exception("%s is not a valid automorf path" % automorf_path)
+
+## def get_bidix_pair(bidix_path):
+##     try:
+##         return re.match('apertium-([^.]*)[.]([^.]*)[.]dix([.]xml', last_component(bidix_path)).group(1)
+##     except:
+##         raise Exception("%s is not a valid bidix path" % bidix_path)
+
+
+
+
+
+def main(automorf_path, bidix_path, lt_proc_path=Globals.lt_proc_path, output=None, side='l'):
+    """usage: find-missing-monodix-entries.py <options>
+
+    <options> are the following
+    
+    --lt_proc_path  - path to lt-proc; the default is /usr/local/bin/lt-proc
+    --automorf_path - path to the morphological analysis file used by lt-proc; e.g. af-en.automorf.bin
+    --bidix_path    - path to the bidix file to be used
+    --side          - either l or r; used to determine which side in the bidix is to be read; the default is l.
+    --output        - filename to dump the data to; this defaults to stdout
+
+    Example:
+      python find-missing-monodix-entries.py --bidix_path=apertium-en-af.en-af.dix.xml \\
+             --automorf_path=/usr/local/share/apertium/apertium-en-af/af-en.automorf.bin --side=r
+
+      Read the bidix 'apertium-en-af.en-af.dix.xml' in the current directory. Use the morphological
+      analyser file '/usr/local/share/apertium/apertium-en-af/af-en.automorf.bin'. Read entries from the
+      right hand side of the bidix. Everything will be written to stdout.
+    """
+
+    Globals.lt_proc_path = lt_proc_path
+    out_file = None
+
+    if output == None:
+        out_file = sys.stdout
+    else:
+        out_file = open(output, 'w')
+
+    doc = load_xml_file(bidix_path)
+    entries = extract_entries(doc, side)
+    missing = find_missing_entries(automorf_path, entries)
+    write_entries_to_file(out_file, missing)
+    out_file.close()
+    
+    exit(0)
+    
+
+if __name__ == '__main__':
+    param_dict = {}
     
     try:
-        automorf, bidix, side = sys.argv[1:]
+        param_re = re.compile("[-][-]([_a-zA-Z0-9]*)[=](.*)$")
+        param_dict = dict((match.group(1), match.group(2)) for match in (param_re.match(p) for p in sys.argv[1:]))
         
-    except ValueError:
-        try:
-            automorf, bidix = sys.argv[1:]
-        except ValueError:
-            usage()
-            exit(1)
+    except Exception, e:
+        print main.__doc__
+        exit(1)
+
+    try:
+        main(**param_dict)
+
+    except Exception, e:
+        print e
+        print main.__doc__
+        exit(1)
+    
+    
 
     
 
