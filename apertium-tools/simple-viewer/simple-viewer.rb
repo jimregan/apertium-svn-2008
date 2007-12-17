f627b2bd282f39094522acc914d67105906a860a
Winterstream <Winterstream@72bbbca6-d526-0410-a7d9-f06f51895060>
2007-12-17 00:19:02 +0000

    Added dbus library for the simple viewer. Added a slight bit of functionality to the viewer.
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium@3039 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium-tools/simple-viewer/simple-viewer.rb
+++ apertium-tools/simple-viewer/simple-viewer.rb
@@ -1,10 +1,50 @@
 require 'libglade2'
+require 'dbus'
 
 class MainWindow
+  def setup_combo(combo)
+    combo.set_model(Gtk::ListStore.new(String))
+    cell = Gtk::CellRendererText.new()
+    combo.pack_start(cell, true)
+    combo.add_attribute(cell, "text", 0)
+    combo
+  end
 
   def initialize(path)
     @glade = GladeXML.new(path) {|handler| method(handler)}
     @app_window = @glade.get_widget("app_window")
+    @bus = DBus::SessionBus.instance
+
+    @info = @bus.introspect("org.apertium.info", "/")["org.apertium.Info"]
+    @translate = @bus.introspect("org.apertium.mode", "/")["org.apertium.Mode"]
+
+    @comboPair = self.setup_combo(@glade["comboPair"])
+    @info.modes.each do |mode|
+      @comboPair.append_text(mode)
+    end
+    @comboPair.set_active(0)
+
+    @buffer = @glade["txtInput"].buffer
+    @buffer.signal_connect("changed") do |w|
+      
+
+      if @user_action
+        @undopool <<  ["insert_text", iter.offset, iter.offset + text.scan(/./).size, text]
+        @redopool.clear
+      end
+    end
+  end
+
+  def on_btnQuit_clicked(*widget)
+    Gtk.main_quit
+  end
+
+  def on_wndMain_destroy(widget)
+    Gtk.main_quit
+  end
+
+  def on_wndMain_delete_event(widget, event)
+    false
   end
 end
 
